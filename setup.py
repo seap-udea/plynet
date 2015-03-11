0b497030196e2da90c4999cf47a53c72534b9f9f
Jorge Zuluaga <zuluagajorge@gmail.com>
2015-03-11 13:28:02 -0500

    Commit

--- setup.py
+++ setup.py
@@ -1,26 +1,50 @@
 ############################################################
-#           $$\                                $$\     
-#           $$ |                               $$ |    
-#  $$$$$$\  $$ |$$\   $$\ $$$$$$$\   $$$$$$\ $$$$$$\   
-# $$  __$$\ $$ |$$ |  $$ |$$  __$$\ $$  __$$\\_$$  _|  
-# $$ /  $$ |$$ |$$ |  $$ |$$ |  $$ |$$$$$$$$ | $$ |    
-# $$ |  $$ |$$ |$$ |  $$ |$$ |  $$ |$$   ____| $$ |$$\ 
-# $$$$$$$  |$$ |\$$$$$$$ |$$ |  $$ |\$$$$$$$\  \$$$$  |
-# $$  ____/ \__| \____$$ |\__|  \__| \_______|  \____/ 
-# $$ |          $$\   $$ |                             
-# $$ |          \$$$$$$  |                             
-# \__|           \______/                            
-#
-# Jorge I. Zuluaga [)] 2015 
+#           $$\                                $$\         #
+#           $$ |                               $$ |        #
+#  $$$$$$\  $$ |$$\   $$\ $$$$$$$\   $$$$$$\ $$$$$$\       #
+# $$  __$$\ $$ |$$ |  $$ |$$  __$$\ $$  __$$\\_$$  _|      #
+# $$ /  $$ |$$ |$$ |  $$ |$$ |  $$ |$$$$$$$$ | $$ |        #
+# $$ |  $$ |$$ |$$ |  $$ |$$ |  $$ |$$   ____| $$ |$$\     #
+# $$$$$$$  |$$ |\$$$$$$$ |$$ |  $$ |\$$$$$$$\  \$$$$  |    #
+# $$  ____/ \__| \____$$ |\__|  \__| \_______|  \____/     #
+# $$ |          $$\   $$ |                                 #
+# $$ |          \$$$$$$  |                                 #
+# \__|           \______/                                  #
+#                                                          #
+# Jorge I. Zuluaga [)] 2015                                #
 ############################################################
 # Package Setup
 ############################################################
-
 from setuptools import setup, find_packages
-from os import path
+from distutils.command.install_scripts import install_scripts
+from os import path,system
+import sys
+
+class install_data(install_scripts):
+    def run(self):
+        install_scripts.run(self)
+        print "Creating plynet_data...",
+        system("mkdir -p /opt/plynet_data")
+        print "Done."
 
 setup(
     name        = "plynet",
-    version     = "alpha-0.1",
-    description = "Python Planetary Physics Package"
+    version     = "0.1.0a1",
+    description = "Python Planetary Physics Package",
+    url         = "http://github.com/seap-udea/plynet",
+    author      = "Jorge I. Zuluaga",
+    author_email= "jorge.zuluaga@udea.edu.co",
+    license     = "GPL2.0",
+    classifiers = [
+        "Development status :: 1 - Planning",
+        "Intended audience :: Scientist",
+        "Topic :: Scientific/Engineering :: Astronomy",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Programming Language :: Python :: 2.6"
+        ],
+    keywords    = "plynet planets",
+    packages    = find_packages(exclude=['contrib','docs','tests*']),
+    data_files  = [('data',['/opt/plynet_data'])],
+    scripts     = ['plynet_check'],
+    cmdclass    = {'install_scripts':install_data}
 )
